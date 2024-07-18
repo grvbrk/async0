1cd9ef54d5f65657949cbb4a3c6dd8fa6b2ed392
grvbrk <gouravbarikofficial@gmail.com>
2024-07-18 12:32:26 +0530

    Minor index.ts code changes

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,15 +1,19 @@
+import { ComponentType } from "react";
 import DuplicateInteger from "./problem-ui/DuplicateInteger";
 import IsAnagram from "./problem-ui/isAnagram";
 
-export function unescapeCode(code: string): string {
-  return code
-    .replace(/\\r\\n/g, "\n") // Replace \r\n with actual newline
-    .replace(/\\\//g, "/") // Replace \/ with /
-    .replace(/\\n/g, "\n") // Replace \n with actual newline
-    .replace(/\\t/g, "\t") // Replace \t with actual tab
-    .replace(/\\'/g, "'") // Replace \' with '
-    .replace(/\\"/g, '"') // Replace \" with "
-    .replace(/\\\\/g, "\\"); // Replace \\ with \
+export function unescapeCode(code: string | undefined): string | void {
+  if (code === undefined) return;
+  else {
+    return code
+      .replace(/\\r\\n/g, "\n")
+      .replace(/\\\//g, "/")
+      .replace(/\\n/g, "\n")
+      .replace(/\\t/g, "\t")
+      .replace(/\\'/g, "'")
+      .replace(/\\"/g, '"')
+      .replace(/\\\\/g, "\\");
+  }
 }
 
 export function escapeCode(code: string): string {
@@ -23,7 +27,24 @@ export function escapeCode(code: string): string {
     .replace(/\//g, "\\/"); // Replace / with \/
 }
 
-export const problemComponents: Record<string, () => JSX.Element> = {
+export const problemDescriptions: Record<string, ComponentType<any>> = {
   "Duplicate Integer": DuplicateInteger,
   "Is Anagram": IsAnagram,
+};
+
+export type judge0TokenResponseType = {
+  status: string;
+  value: judge0ValueKeyType;
+};
+
+export type judge0ValueKeyType = {
+  stdout: string | null;
+  time: string;
+  memory: number;
+  stderr: string | null;
+  token: string;
+  compile_output: string | null;
+  message: string | null;
+  status: { id: number; description: string };
+  output?: string;
 };
