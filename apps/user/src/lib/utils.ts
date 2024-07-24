5b1047cb2d13515337996ae55205b9f088db98bb
grvbrk <gouravbarikofficial@gmail.com>
2024-07-24 20:20:19 +0530

    init constants

--- apps/user/src/lib/utils.ts
+++ apps/user/src/lib/utils.ts
@@ -1,6 +1,1 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const MAX_QUERY_LENGTH = 20;
