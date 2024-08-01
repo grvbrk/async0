469a90b814a21238413626b2127eb9b43422aa34
grvbrk <gouravbarikofficial@gmail.com>
2024-08-01 22:16:06 +0530

    visibility constant

--- apps/user/src/lib/utils.ts
+++ apps/user/src/lib/utils.ts
@@ -1,1 +1,43 @@
 export const MAX_QUERY_LENGTH = 20;
+export const COLUMN_VISIBILITY_BREAKPOINTS = [
+  {
+    max: 640,
+    columns: {
+      solutions: false,
+      difficulty: false,
+      topics: false,
+      lists: false,
+      link: false,
+    },
+  },
+  {
+    max: 768,
+    columns: {
+      solutions: false,
+      difficulty: true,
+      topics: true,
+      lists: false,
+      link: false,
+    },
+  },
+  {
+    max: 1024,
+    columns: {
+      solutions: true,
+      difficulty: true,
+      topics: true,
+      lists: false,
+      link: false,
+    },
+  },
+  {
+    max: Infinity,
+    columns: {
+      solutions: true,
+      difficulty: true,
+      topics: true,
+      lists: true,
+      link: true,
+    },
+  },
+];
