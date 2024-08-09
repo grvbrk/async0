2c78d04132784b03510adb11a786e7c0b12802f0
grvbrk <gouravbarikofficial@gmail.com>
2024-08-09 20:33:41 +0530

    Navbar responsive

--- apps/user/src/app/_components/Navbar.tsx
+++ apps/user/src/app/_components/Navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   signIn,
   signOut,
@@ -8,7 +9,6 @@ import {
   LiteralUnion,
 } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { Input } from "@repo/ui/components/ui/input";
 import { Button } from "@repo/ui/components/ui/button";
 import {
   DropdownMenu,
@@ -20,7 +20,7 @@ import {
 } from "@repo/ui/components/ui/dropdown-menu";
 
 import Link from "next/link";
-import { Loader2, LogIn, Search } from "lucide-react";
+import { Loader2, LogIn } from "lucide-react";
 import { BuiltInProviderType } from "next-auth/providers/index";
 import {
   Avatar,
@@ -32,12 +32,14 @@ import Sidebar from "../problems/_components/Sidebar";
 import { useTheme } from "next-themes";
 import { Switch } from "@repo/ui/components/ui/switch";
 import { Skeleton } from "@repo/ui/components/ui/skeleton";
-import { useDebounce } from "../hooks/useDebounce";
-import { MAX_QUERY_LENGTH } from "@/lib/utils";
-import { toast } from "sonner";
-import { searchProblemQuery } from "../actions/search";
-import SearchDisplay from "./SearchDisplay";
+
 import Searchbar from "./Searchbar";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@repo/ui/components/ui/accordion";
 
 type ProvidersResponse = Record<
   LiteralUnion<BuiltInProviderType, string>,
@@ -49,37 +51,22 @@ export default function Navbar() {
   const { theme, setTheme } = useTheme();
   const pathname = usePathname();
   const [providers, setProviders] = useState<ProvidersResponse | null>(null);
-  const [mounted, setMounted] = useState(false);
   const [auth0Authenticated, setAuth0Authenticated] = useState(false);
-  const [text, setText] = useState<string>("");
-  const [searchResults, setSearchResults] = useState<any[]>([]);
-  const debouncedText = useDebounce(text, 0.5);
-  const [showCard, setShowCard] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchData() {
-      if (debouncedText.length >= 0) {
-        try {
-          const results = await searchProblemQuery(debouncedText);
-          setSearchResults(results as any[]);
-        } catch (error) {
-          console.error("Error searching problems:", error);
-        }
-      }
       try {
         const response = await getProviders();
         setProviders(response);
       } catch (error) {
         console.error("Error getting providers:", error);
       }
-
-      setMounted(true);
     }
 
     fetchData();
-  }, [debouncedText]);
+  }, []);
 
-  if (!mounted || status === "loading")
+  if (status === "loading")
     return (
       <div className="sticky top-0 w-full h-16 flex items-center justify-between gap-4 border-b px-4 md:px-6 z-10 bg-background">
         <div className="flex items-center gap-4">
@@ -99,85 +86,89 @@ export default function Navbar() {
   return (
     <>
       <header className="sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6 z-10">
-        <nav className="w-full flex gap-10 pl-2 text-sm md:pl-4 md:flex md:flex-row md:items-center md:gap-10 lg:gap-12 ">
-          {pathname != "/" && (
-            <Link
-              href="#"
-              className="hidden md:block md:font-bold md:transition-colors md:hover:text-primary md:text-muted-foreground"
-            >
-              <Sidebar />
-            </Link>
-          )}
+        <div className="flex gap-10 pl-2 text-sm md:pl-4 md:flex md:flex-row md:items-center md:gap-8 lg:gap-12">
+          <Link
+            href="#"
+            className=" font-bold transition-colors hover:text-primary text-muted-foreground"
+          >
+            <Sidebar />
+          </Link>
 
           <Link
             href="/"
-            className={`md:block font-black text-foreground transition-colors hover:text-primary ${pathname === "/" && "text-primary"}`}
+            className={`hidden md:block font-black text-foreground transition-colors hover:text-primary ${pathname === "/" && "text-primary"}`}
           >
             Async0
           </Link>
 
           <Link
             href="/problems"
-            className={`md:block text-muted-foreground transition-colors hover:text-primary ${pathname === "/problems" && "text-primary"}`}
+            className={`hidden md:block text-muted-foreground transition-colors hover:text-primary ${pathname === "/problems" && "text-primary"}`}
           >
             Problems
           </Link>
 
           <Link
             href="/neetcode"
-            className={`md:block text-muted-foreground transition-colors hover:text-primary ${pathname === "/neetcode" && " text-primary"}`}
+            className={`hidden md:block text-muted-foreground transition-colors hover:text-primary ${pathname === "/neetcode" && " text-primary"}`}
           >
             Neetcode
           </Link>
-        </nav>
-        <Searchbar className="hidden lg:block" />
+        </div>
 
-        <Switch
-          id="toggleMode"
-          checked={theme === "dark"}
-          onCheckedChange={() =>
-            theme === "light" ? setTheme("dark") : setTheme("light")
-          }
-        />
+        <div className="flex items-center gap-4 w-full">
+          <div className="w-full lg:w-[450px] lg:ml-auto">
+            <Searchbar />
+          </div>
+          <div className="flex items-center gap-4">
+            <Switch
+              id="toggleMode"
+              checked={theme === "dark"}
+              onCheckedChange={() =>
+                theme === "light" ? setTheme("dark") : setTheme("light")
+              }
+            />
 
-        {auth0Authenticated ? (
-          <Button disabled>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Signing in...
-          </Button>
-        ) : session ? (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Avatar className="h-9 w-9 cursor-pointer">
-                <AvatarImage src={session.user?.image!} alt="pfp" />
-                <AvatarFallback>CN</AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={() => signOut()}>
-                Logout
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        ) : (
-          providers &&
-          Object.values(providers).map((provider, idx) => {
-            return (
-              <Button
-                key={idx}
-                onClick={() => {
-                  setAuth0Authenticated(true);
-                  signIn(provider.id);
-                }}
-              >
-                <LogIn size="16" className="mr-2" />
-                Sign in
+            {auth0Authenticated ? (
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Signing in...
               </Button>
-            );
-          })
-        )}
+            ) : session ? (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Avatar className="h-9 w-9 cursor-pointer">
+                    <AvatarImage src={session.user?.image!} alt="pfp" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem onClick={() => signOut()}>
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            ) : (
+              providers &&
+              Object.values(providers).map((provider, idx) => {
+                return (
+                  <Button
+                    key={idx}
+                    onClick={() => {
+                      setAuth0Authenticated(true);
+                      signIn(provider.id);
+                    }}
+                  >
+                    <LogIn size="16" className="mr-2" />
+                    Sign in
+                  </Button>
+                );
+              })
+            )}
+          </div>
+        </div>
       </header>
     </>
   );
